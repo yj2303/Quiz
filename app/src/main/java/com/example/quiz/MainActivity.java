b2f083e25311de461e764b2f79fe3c1201a08727
Inam Khan <inamkh0001@gmail.com>
2020-05-17 23:46:07 +0530

    Added Splash Screen

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -14,14 +14,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button answer1, answer2, answer3, answer4;
-    TextView score, question;
 
-    private Questions mQuestions = new Questions();
-
-    private String mAnswer;
-    private int mScore = 0;
-    private int mQuestionsLength = mQuestions.mQuestions.length;
 
     Random r;
 
@@ -30,106 +23,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-                r = new Random();
-
-        answer1 = (Button) findViewById(R.id.answer1);
-        answer2 = (Button) findViewById(R.id.answer2);
-        answer3 = (Button) findViewById(R.id.answer3);
-        answer4 = (Button) findViewById(R.id.answer4);
-
-        score = (TextView) findViewById(R.id.score);
-        question = (TextView) findViewById(R.id.question);
-
-        score.setText("Score:" + mScore);
-        updateQuestion(r.nextInt(mQuestionsLength));
-
-        answer1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            if(answer1.getText() == mAnswer){
-                mScore++;
-                score.setText("Score:" + mScore);
-                updateQuestion(r.nextInt(mQuestionsLength));
-            }
-            else {
-                gameOver();
-            }
-            }
-        });
-        answer2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(answer2.getText() == mAnswer){
-                    mScore++;
-                    score.setText("Score:" + mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
-                }
-                else {
-                    gameOver();
-                }
-
-            }
-        });
-        answer3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(answer3.getText() == mAnswer){
-                    mScore++;
-                    score.setText("Score:" + mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
-                }
-                else {
-                    gameOver();
-                }
-
-            }
-        });
-        answer4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(answer4.getText() == mAnswer){
-                    mScore++;
-                    score.setText("Score:" + mScore);
-                    updateQuestion(r.nextInt(mQuestionsLength));
-                }
-                else {
-                    gameOver();
-                }
-
-            }
-        });
-
-    }
-
-    private void updateQuestion(int num){
-        question.setText(mQuestions.getQuestion(num));
-        answer1.setText(mQuestions.getChoice1(num));
-        answer2.setText(mQuestions.getChoice2(num));
-        answer3.setText(mQuestions.getChoice3(num));
-        answer4.setText(mQuestions.getChoice4(num));
-
-        mAnswer = mQuestions.getCorrectAnswer(num);
-    }
-    private void gameOver(){
-        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-        alertDialogBuilder
-                .setMessage("Game Over! Your Score is" +mScore + "points.")
-                .setCancelable(false)
-                .setPositiveButton("NEW GAME",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                    }
-                })
-                .setNegativeButton("EXIT",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                    }
-                    });
-       AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
     }
 }
